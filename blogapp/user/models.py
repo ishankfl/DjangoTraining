c657dc4c67002df575f328ad8a4d5858d416840d
ishankfl <ishankafle5@gmail.com>
2025-02-21 22:57:27 +0545

    hi

--- blogapp/user/models.py
+++ blogapp/user/models.py
@@ -1,13 +1,21 @@
-from django.db import models
-
-# Create your models here.
+# from django.db import models  # Importing Django's models module
+# # from django.db
+# # Defining the User model
+# class User(models.Model):
+#     name = models.CharField(max_length=255)  # Character field for storing the name
+#     email = models.EmailField(unique=True)   # Email field with unique constraint
+#     age = models.IntegerField(default=0)              # Integer field to store age
+#     monthly_salary = models.DecimalField(max_digits=10, decimal_places=2)  # Decimal field for salary
+#     max_integer = models.BigIntegerField()
+    
+#     def __str__(self):
+#         return self.name  # String representation of the model
+ 
+from  django.db import models
 class User(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length= 50)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField(default=20)
 
-
-# User
-
-#columns: name, email, created_at
-# attributes 
+    def __str__(self):
+        return self.name
